d7b37fd1b7aad6b090c25c24f23cb61e3b57cedb
Kruty1918 <131464148+Kruty1918@users.noreply.github.com>
2024-12-21 00:50:11 +0100

    added basic logic for creating and joining rooms

--- Assets/Scripts/Network/ConnectToServer.cs
+++ Assets/Scripts/Network/ConnectToServer.cs
@@ -1,13 +1,24 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Kruty1918.Networking
 {
-    public class ConnectToServer : MonoBehaviour
+    public class ConnectToServer : MonoBehaviourPunCallbacks
     {
         void Start()
         {
             PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            SceneManager.LoadScene("Lobby");
         }
     }
 }
